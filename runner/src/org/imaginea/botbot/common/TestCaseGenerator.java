82ba48b2ea923a557ef54c6a0197499baf0bd9e5
Varun Menon <mr.varun.menon@gmail.com>
2012-04-26 20:41:03 +0530

    Fixing the issue file separator of windows machines.

--- runner/src/org/imaginea/botbot/common/TestCaseGenerator.java
+++ runner/src/org/imaginea/botbot/common/TestCaseGenerator.java
@@ -2,239 +2,275 @@ package org.imaginea.botbot.common;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import junit.framework.Assert;
 
 import org.imaginea.botbot.api.DefaultProperties;
 
 public class TestCaseGenerator {
-	HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
 
-	public void listDirectory(File f, String directoryName,
-			HashMap<String, ArrayList<String>> fileList) {
+    HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
+    private static String baseFolder="";
+    //private static String baseFolder=new File("runner").getPath()+File.separatorChar;
+    public void listDirectory(File f, String directoryName,
+            HashMap<String, ArrayList<String>> fileList) {
+        String testcaseFolder="testcases"+File.separatorChar;
+        File[] listOfFiles = f.listFiles();
+        if (directoryName.equalsIgnoreCase("") && f.isDirectory()) {
+            directoryName = f.getPath().split(testcaseFolder)[1];
+        } else if (directoryName.equalsIgnoreCase("") && f.isFile()) {
+            directoryName = new File(f.getParent()).getPath().split(
+                    testcaseFolder)[1];
+        }
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()
+&& listOfFiles[i].getName().endsWith(".csv")
+&& !listOfFiles[i].getName().startsWith(".")) {
+                if (fileList.containsKey(directoryName)) {
+                    fileList.get(directoryName).add(
+                            listOfFiles[i].getAbsolutePath());
+                } else {
+                    ArrayList<String> ar = new ArrayList<String>();
+                    ar.add(listOfFiles[i].getAbsolutePath());
+                    fileList.put(directoryName, ar);
+                }
+            } else if (listOfFiles[i].isDirectory()) {
 
-		File[] listOfFiles = f.listFiles();
-		if (directoryName.equalsIgnoreCase("") && f.isDirectory()) {
-			directoryName = f.getPath().split("testcases/")[1];
-		} else if (directoryName.equalsIgnoreCase("") && f.isFile()) {
-			directoryName = new File(f.getParent()).getPath().split(
-					"testcases/")[1];
-		}
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()
-					&& listOfFiles[i].getName().endsWith(".csv")
-					&& !listOfFiles[i].getName().startsWith(".")) {
-				if (fileList.containsKey(directoryName)) {
-					fileList.get(directoryName).add(
-							listOfFiles[i].getAbsolutePath());
-				} else {
-					ArrayList<String> ar = new ArrayList<String>();
-					ar.add(listOfFiles[i].getAbsolutePath());
-					fileList.put(directoryName, ar);
-				}
-			} else if (listOfFiles[i].isDirectory()) {
+                listDirectory(listOfFiles[i],
+                        listOfFiles[i].getPath().split(testcaseFolder)[1],
+                        fileList);
+            }
+        }
+    }
 
-				listDirectory(listOfFiles[i],
-						listOfFiles[i].getPath().split("testcases/")[1],
-						fileList);
-			}
-		}
-	}
+    public void listAbsoluteDirectory(File f, String directoryName,    HashMap<String, ArrayList<String>> fileList) {
+        char pathSeparator=File.separatorChar;
+        String testcaseFolder=baseFolder+"testcases"+pathSeparator;
+        File[] listOfFiles = f.listFiles();
 
-	public void listAbsoluteDirectory(File f, String directoryName,
-			HashMap<String, ArrayList<String>> fileList) {
+        if (directoryName.equalsIgnoreCase("") && f.isDirectory()) {
+            String path=f.getPath();
+                directoryName=getDirectoryName(testcaseFolder,path);
+        } else if (directoryName.equalsIgnoreCase("") && f.isFile()) {
+            directoryName = getDirectoryName(testcaseFolder,new File(f.getParent()).getPath()                 );
+        }
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()
+&& listOfFiles[i].getName().endsWith(".csv")
+&& !listOfFiles[i].getName().startsWith(".")) {
+                if (fileList.containsKey(directoryName)) {
+                    fileList.get(directoryName).add(
+                            listOfFiles[i].getPath().replace("\\", "/"));
+                } else {
+                    ArrayList<String> ar = new ArrayList<String>();
+                    ar.add(listOfFiles[i].getPath().replace("\\", "/"));
+                    fileList.put(directoryName, ar);
+                }
+            } else if (listOfFiles[i].isDirectory()) {
+                listAbsoluteDirectory(listOfFiles[i],
+                        getDirectoryName(testcaseFolder,listOfFiles[i].getPath()),
+                        fileList);
+            }
+        }
+    }
+    private String getDirectoryName(String path,String testFolder){
+        String directoryName = "";
+        if (String.valueOf(File.separatorChar).contentEquals("\\")) {
+            List<String> paths = new ArrayList<String>(Arrays.asList(path.split("\\\\")));
+            List<String> testCaseFolders = new ArrayList<String>(Arrays
+                    .asList(testFolder.split("\\\\")));
+            int pathsLength = paths.size();
+            for (int i = 0; i < pathsLength && paths.size() != 0; i++) {
+                if (!paths.get(0).contentEquals(testCaseFolders.get(0))) {
+                    break;
+                }
+                paths.remove(0);
+                testCaseFolders.remove(0);
+            }
+            Iterator<String> it = testCaseFolders.iterator();
+            while (it.hasNext()) {
+                directoryName = directoryName + File.separatorChar + it.next();
+            }
+            directoryName = directoryName.substring(1);
+        } else {
+            directoryName = testFolder.split(path)[1];
+        }
+        return directoryName;
+    }
 
-		File[] listOfFiles = f.listFiles();
-		if (directoryName.equalsIgnoreCase("") && f.isDirectory()) {
-			directoryName = f.getPath().split("testcases/")[1];
-		} else if (directoryName.equalsIgnoreCase("") && f.isFile()) {
-			directoryName = new File(f.getParent()).getPath().split(
-					"testcases/")[1];
-		}
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()
-					&& listOfFiles[i].getName().endsWith(".csv")
-					&& !listOfFiles[i].getName().startsWith(".")) {
-				if (fileList.containsKey(directoryName)) {
-					fileList.get(directoryName).add(
-							listOfFiles[i].getPath());
-				} else {
-					ArrayList<String> ar = new ArrayList<String>();
-					ar.add(listOfFiles[i].getPath());
-					fileList.put(directoryName, ar);
-				}
-			} else if (listOfFiles[i].isDirectory()) {
+    public void listDirectory1(File f, ArrayList<File> fileList) {
+        File[] listOfFiles = f.listFiles();
 
-				listAbsoluteDirectory(listOfFiles[i],
-						listOfFiles[i].getPath().split("testcases/")[1],
-						fileList);
-			}
-		}
-	}
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                fileList.add(listOfFiles[i]);
+            } else if (listOfFiles[i].isDirectory()) {
+                listDirectory1(listOfFiles[i], fileList);
+            }
+        }
+    }
 
-	
-	public void listDirectory1(File f, ArrayList<File> fileList) {
-		File[] listOfFiles = f.listFiles();
+    public void classGenerator(HashMap<String, ArrayList<String>> hm,String template)
+            throws Exception {
+        char pathSeparator= File.separatorChar;
+        Iterator<String> it = hm.keySet().iterator();
+        String testSource=baseFolder+("src:test:".replace(":", String.valueOf(pathSeparator)));
+        while (it.hasNext()) {
+            String classPath = it.next();
+            String packagePath = classPath.toLowerCase();
 
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) {
-				fileList.add(listOfFiles[i]);
-			} else if (listOfFiles[i].isDirectory()) {
-				listDirectory1(listOfFiles[i], fileList);
-			}
-		}
-	}
+            packagePath = replaceSpecialChar(packagePath);
+            String javaClassFile = new File(packagePath).getName();
+            String javaClassName = javaClassFile.substring(0, 1).toUpperCase()
+                    + javaClassFile.substring(1);
+            try{
+            packagePath = packagePath
+                    .substring(0, packagePath.lastIndexOf(pathSeparator));}
+            catch(StringIndexOutOfBoundsException e)
+            {
+                System.out.println("Exception out of bounds");
+                packagePath="";
+            }
+            new File(testSource + packagePath).mkdirs();
+            Writer output = new BufferedWriter(new FileWriter(testSource
+                    + packagePath + pathSeparator + javaClassName + ".java"));
+            ArrayList<String> ar = hm.get(classPath);
 
-	public void classGenerator(HashMap<String, ArrayList<String>> hm,String template)
-			throws Exception {
+            classWriter(classPath, ar, output,template);
+            output.close();
+        }
 
-		Iterator<String> it = hm.keySet().iterator();
+    }
 
-		while (it.hasNext()) {
-			String classPath = it.next();
-			String packagePath = classPath.toLowerCase();
+    public static void main(String[] args) {
+        TestCaseGenerator tc = new TestCaseGenerator();
+        HashMap<String, ArrayList<String>> hm1 = new HashMap<String, ArrayList<String>>();
+        DefaultProperties prop = DefaultProperties.getDefaultProperty();
+        boolean isRobotium=false;
+        String template ="Template";
+        char pathSeparator=File.separatorChar;
+        File f =new File("");
+        System.out.println(f.getAbsolutePath());
+        String testFolderPath=baseFolder+"testcases"+pathSeparator;
+        if(prop.getValueFromProperty("FRAMEWORK").equalsIgnoreCase("robotium")){
+            isRobotium=true;
+            template="RobotiumTemplate";
+        }
+        if(isRobotium){
+            tc.listAbsoluteDirectory(new File(testFolderPath
+                    + prop.getValueFromProperty("TESTCASE_FOLDER")), "",
+            hm1);
+        }
+        else{
+            tc.listDirectory(
+                    new File(testFolderPath
+                            + prop.getValueFromProperty("TESTCASE_FOLDER")), "",
+                    hm1);
 
-			packagePath = replaceSpecialChar(packagePath);
-			String javaClassFile = new File(packagePath).getName();
-			String javaClassName = javaClassFile.substring(0, 1).toUpperCase()
-					+ javaClassFile.substring(1);
-			try{
-			packagePath = packagePath
-					.substring(0, packagePath.lastIndexOf("/"));}
-			catch(StringIndexOutOfBoundsException e)
-			{
-				System.out.println("Exception out of bounds");
-				packagePath="";
-			}
-			new File("src/test/" + packagePath).mkdirs();
-			Writer output = new BufferedWriter(new FileWriter("src/test/"
-					+ packagePath + "/" + javaClassName + ".java"));
-			ArrayList<String> ar = hm.get(classPath);
+        }
+        System.out.println(hm1);
+        try {
+            tc.classGenerator(hm1,template);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			classWriter(classPath, ar, output,template);
-			output.close();
-		}
+    private void classWriter(String classPath, ArrayList<String> ar,
+            Writer output,String template) {
+        boolean isRobotium=false;
+        FileInputStream fstream;
+        char pathSeparator=File.separatorChar;
+        String resourcesPath=baseFolder+"resources"+pathSeparator;
+        if(template.contentEquals("RobotiumTemplate")){
+            isRobotium=true;
+        }
+        try {
+            String tempString = "";
+            String importString = classPath.replace(String.valueOf(pathSeparator), ".");
+            String outputStringClass="";
+            importString = replaceSpecialChar(importString);
+            try{
+            importString = importString.substring(0,
+                    importString.lastIndexOf("."));
+            }
+            catch(StringIndexOutOfBoundsException e)
+            {
+                System.out.println("Exception out of bounds");
+                outputStringClass = "package test;\n";
+            }
+            if(outputStringClass==""){
+            outputStringClass = "package test." + importString + ";\n";
+            }
+            String outputStringTestCase = "";
+            String testCase = "";
 
-	}
+            fstream = new FileInputStream(resourcesPath+template);
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            boolean flag = false;
+            while ((tempString = br.readLine()) != null) {
+                if (tempString.equalsIgnoreCase("@Test") || (isRobotium && tempString.contains("testName")))
+                    flag = true;
 
-	public static void main(String[] args) {
-		TestCaseGenerator tc = new TestCaseGenerator();
-		HashMap<String, ArrayList<String>> hm1 = new HashMap<String, ArrayList<String>>();
-		DefaultProperties prop = DefaultProperties.getDefaultProperty();
-		boolean isRobotium=false;
-		String template ="Template";
-		if(prop.getValueFromProperty("FRAMEWORK").equalsIgnoreCase("robotium")){
-			isRobotium=true;
-			template="RobotiumTemplate";
-		}
-		if(isRobotium){
-			tc.listAbsoluteDirectory(new File("testcases/"
-					+ prop.getValueFromProperty("TESTCASE_FOLDER")), "",
-			hm1);
-		}
-		else{
-			tc.listDirectory(
-					new File("testcases/"
-							+ prop.getValueFromProperty("TESTCASE_FOLDER")), "",
-					hm1);
-				
-		}
-		System.out.println(hm1);
-		try {
-			tc.classGenerator(hm1,template);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                if (!flag)
+                    outputStringClass += tempString + "\n";
+                else
+                    outputStringTestCase += tempString + "\n";
 
-	private void classWriter(String classPath, ArrayList<String> ar,
-			Writer output,String template) {
-		boolean isRobotium=false;
-		FileInputStream fstream;
-		if(template.contentEquals("RobotiumTemplate")){
-			isRobotium=true;
-		}
-		try {
-			String tempString = "";
-			String importString = classPath.replace("/", ".");
-			String outputStringClass="";
-			importString = replaceSpecialChar(importString);
-			try{
-			importString = importString.substring(0,
-					importString.lastIndexOf("."));
-			}
-			catch(StringIndexOutOfBoundsException e)
-			{
-				System.out.println("Exception out of bounds");
-				outputStringClass = "package test;\n";
-			}
-			if(outputStringClass==""){
-			outputStringClass = "package test." + importString + ";\n";
-			}
-			String outputStringTestCase = "";
-			String testCase = "";
+            }
+            String packagePath = classPath.toLowerCase();
+            String javaClassFile = new File(packagePath).getName();
+            javaClassFile = replaceSpecialChar(javaClassFile);
+            String javaClassName = javaClassFile.substring(0, 1).toUpperCase()
+                    + javaClassFile.substring(1);
 
-			fstream = new FileInputStream("resources/"+template);
-			DataInputStream in = new DataInputStream(fstream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			boolean flag = false;
-			while ((tempString = br.readLine()) != null) {
-				if (tempString.equalsIgnoreCase("@Test") || (isRobotium && tempString.contains("testName")))
-					flag = true;
+            outputStringClass = outputStringClass.replace("TestClassName",
+                    javaClassName);
+            output.write(outputStringClass + "\n");
 
-				if (!flag)
-					outputStringClass += tempString + "\n";
-				else
-					outputStringTestCase += tempString + "\n";
+            Iterator<String> arrayIt = ar.iterator();
+            while (arrayIt.hasNext()) {
+                String testfile = arrayIt.next();
+                String fileName = new File(testfile).getName();
+                fileName = fileName.substring(0, fileName.indexOf(".csv"));
 
-			}
-			String packagePath = classPath.toLowerCase();
-			String javaClassFile = new File(packagePath).getName();
-			javaClassFile = replaceSpecialChar(javaClassFile);
-			String javaClassName = javaClassFile.substring(0, 1).toUpperCase()
-					+ javaClassFile.substring(1);
+                fileName = replaceSpecialChar(fileName);
+                if(isRobotium){
+                    fileName="test"+fileName.substring(0, 1).toUpperCase()
+                            + fileName.substring(1);
+                }
 
-			outputStringClass = outputStringClass.replace("TestClassName",
-					javaClassName);
-			output.write(outputStringClass + "\n");
+                testCase = outputStringTestCase.replace("testName", fileName);
+                testCase = testCase.replace("filePath", testfile);
 
-			Iterator<String> arrayIt = ar.iterator();
-			while (arrayIt.hasNext()) {
-				String testfile = arrayIt.next();
-				String fileName = new File(testfile).getName();
-				fileName = fileName.substring(0, fileName.indexOf(".csv"));
+                output.write(testCase + "\n");
+            }
 
-				fileName = replaceSpecialChar(fileName);
-				if(isRobotium){
-					fileName="test"+fileName.substring(0, 1).toUpperCase()
-							+ fileName.substring(1);
-				}
+            output.write("}");
 
-				testCase = outputStringTestCase.replace("testName", fileName);
-				testCase = testCase.replace("filePath", testfile);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            Assert.fail(e.toString());
+        }
 
-				output.write(testCase + "\n");
-			}
-
-			output.write("}");
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			Assert.fail(e.toString());
-		}
-
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			Assert.fail(e.toString());
-		}
-	}
-	private String replaceSpecialChar(String fileName)
-	{
-		fileName=fileName.replace(" ", "");
-		fileName=fileName.replace("-", "");
-		fileName=fileName.replace("_", "");
-		return fileName;
-	}
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            Assert.fail(e.toString());
+        }
+    }
+    private String replaceSpecialChar(String fileName)
+    {
+        fileName=fileName.replace(" ", "");
+        fileName=fileName.replace("-", "");
+        fileName=fileName.replace("_", "");
+        return fileName;
+    }
 }
+
+
+
