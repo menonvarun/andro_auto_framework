d50b3a9ce16e2787886969f1cf88199b1456c389
Moiz Virani <moizjv@gmail.com>
2012-05-04 20:08:31 +0530

    Changes on code base to support data driven

--- runner/src/org/imaginea/botbot/common/TestCaseGenerator.java
+++ runner/src/org/imaginea/botbot/common/TestCaseGenerator.java
@@ -11,72 +11,79 @@ import junit.framework.Assert;
 
 import org.imaginea.botbot.api.DefaultProperties;
 
+
+
 public class TestCaseGenerator {
 
     HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
     private static String baseFolder="";
+    private String dataDrivenClasses = "";
     //private static String baseFolder=new File("runner").getPath()+File.separatorChar;
-    public void listDirectory(File f, String directoryName,
-            HashMap<String, ArrayList<String>> fileList) {
-        String testcaseFolder="testcases"+File.separatorChar;
-        File[] listOfFiles = f.listFiles();
-        if (directoryName.equalsIgnoreCase("") && f.isDirectory()) {
-            directoryName = f.getPath().split(testcaseFolder)[1];
-        } else if (directoryName.equalsIgnoreCase("") && f.isFile()) {
-            directoryName = new File(f.getParent()).getPath().split(
-                    testcaseFolder)[1];
-        }
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()
-&& listOfFiles[i].getName().endsWith(".csv")
-&& !listOfFiles[i].getName().startsWith(".")) {
-                if (fileList.containsKey(directoryName)) {
-                    fileList.get(directoryName).add(
-                            listOfFiles[i].getAbsolutePath());
-                } else {
-                    ArrayList<String> ar = new ArrayList<String>();
-                    ar.add(listOfFiles[i].getAbsolutePath());
-                    fileList.put(directoryName, ar);
-                }
-            } else if (listOfFiles[i].isDirectory()) {
-
-                listDirectory(listOfFiles[i],
-                        listOfFiles[i].getPath().split(testcaseFolder)[1],
-                        fileList);
-            }
-        }
-    }
-
-    public void listAbsoluteDirectory(File f, String directoryName,    HashMap<String, ArrayList<String>> fileList) {
+    public void listDirectory(File f, String directoryName, HashMap<String, ArrayList<String>> fileList,final String fileType, final boolean absolute) {
         char pathSeparator=File.separatorChar;
-        String testcaseFolder=baseFolder+"testcases"+pathSeparator;
+        String testcaseFolder=getPath(new File(baseFolder+"testcases"+pathSeparator),absolute);
         File[] listOfFiles = f.listFiles();
 
         if (directoryName.equalsIgnoreCase("") && f.isDirectory()) {
-            String path=f.getPath();
+            String path=getPath(f,absolute);
                 directoryName=getDirectoryName(testcaseFolder,path);
         } else if (directoryName.equalsIgnoreCase("") && f.isFile()) {
-            directoryName = getDirectoryName(testcaseFolder,new File(f.getParent()).getPath()                 );
+            directoryName = getDirectoryName(testcaseFolder,getPath(new File(f.getParent()),absolute)                 );
         }
         for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()
-&& listOfFiles[i].getName().endsWith(".csv")
-&& !listOfFiles[i].getName().startsWith(".")) {
+            if (shouldBeFiltered(listOfFiles[i], fileType)) {
                 if (fileList.containsKey(directoryName)) {
-                    fileList.get(directoryName).add(
-                            listOfFiles[i].getPath().replace("\\", "/"));
+                    List<String> flist=fileList.get(directoryName);
+                    String fPath=getPath(listOfFiles[i],absolute);
+                    fPath=filterPath(fPath, absolute);
+                    flist.add(fPath);
                 } else {
-                    ArrayList<String> ar = new ArrayList<String>();
-                    ar.add(listOfFiles[i].getPath().replace("\\", "/"));
-                    fileList.put(directoryName, ar);
+                    ArrayList<String> flist = new ArrayList<String>();
+                    String fPath=getPath(listOfFiles[i],absolute);
+                    fPath=filterPath(fPath, absolute);
+                    flist.add(fPath);
+                    fileList.put(directoryName, flist);
                 }
             } else if (listOfFiles[i].isDirectory()) {
-                listAbsoluteDirectory(listOfFiles[i],
-                        getDirectoryName(testcaseFolder,listOfFiles[i].getPath()),
-                        fileList);
+            	listDirectory(listOfFiles[i],
+                        getDirectoryName(testcaseFolder,getPath(listOfFiles[i],absolute)),
+                        fileList,fileType,absolute);
             }
         }
     }
+    
+    private boolean shouldBeFiltered(File f,String filetype){
+    	boolean isRequired=false;
+    	if(f.isDirectory()){
+    		return isRequired;
+    	}
+    	if(f.getName().startsWith(".")){
+    		return isRequired;
+    	}
+    	if(f.getName().endsWith("."+filetype)){
+    		return true;
+    	}
+    	return isRequired;
+    }
+    
+    private String getPath(File f, boolean absolute){
+    	if(absolute){
+    		return f.getAbsolutePath();
+    	}else{
+    		return f.getPath();
+    	}
+    }
+    
+    private String filterPath(String fPath,boolean absolute){
+    	String path;
+    	if(absolute){
+    		path=fPath.replace("\\", "\\\\");
+        }else{
+        	path=fPath.replace("\\", "/");
+        }
+    	return path;
+    }
+    
     private String getDirectoryName(String path,String testFolder){
         String directoryName = "";
         if (String.valueOf(File.separatorChar).contentEquals("\\")) {
@@ -97,22 +104,11 @@ public class TestCaseGenerator {
             }
             directoryName = directoryName.substring(1);
         } else {
-            directoryName = testFolder.split(path)[1];
+            directoryName = testFolder.split(path+File.separatorChar)[1];
         }
         return directoryName;
     }
 
-    public void listDirectory1(File f, ArrayList<File> fileList) {
-        File[] listOfFiles = f.listFiles();
-
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                fileList.add(listOfFiles[i]);
-            } else if (listOfFiles[i].isDirectory()) {
-                listDirectory1(listOfFiles[i], fileList);
-            }
-        }
-    }
 
     public void classGenerator(HashMap<String, ArrayList<String>> hm,String template)
             throws Exception {
@@ -127,6 +123,63 @@ public class TestCaseGenerator {
             String javaClassFile = new File(packagePath).getName();
             String javaClassName = javaClassFile.substring(0, 1).toUpperCase()
                     + javaClassFile.substring(1);
+            
+            //checking if folder name is datadriven 
+			if (javaClassFile.equalsIgnoreCase("datadriven")&&template.contentEquals("RobotiumTemplate")) {
+				try {
+					packagePath = packagePath.substring(0,
+							packagePath.lastIndexOf(pathSeparator));
+				} catch (StringIndexOutOfBoundsException e) {
+					System.out.println("Exception out of bounds");
+					packagePath = "";
+				}
+				//creating package folder 
+				new File(testSource + packagePath + pathSeparator
+						+ javaClassFile).mkdirs();
+				
+				//For iterating through classes 
+				
+				ArrayList<String> ar = hm.get(classPath);
+				
+				
+
+				Iterator<String> dataDrivenIterator = ar.iterator();
+				while (dataDrivenIterator.hasNext()) {
+					String testFilePath = dataDrivenIterator.next();
+					String fileName = new File(testFilePath).getName();
+					fileName = fileName.substring(0, fileName.indexOf(".csv"));
+					
+					//Creating java classes for data driven test cases & ignoring other files 
+					if (fileName.endsWith("_datadriven")) {
+						String dataDrivenJavaFileName = fileName.replace(
+								"_datadriven", "");
+						dataDrivenJavaFileName = dataDrivenJavaFileName
+								.substring(0, 1).toUpperCase()
+								+ dataDrivenJavaFileName.substring(1);
+						Writer output = new BufferedWriter(new FileWriter(
+								testSource + packagePath + pathSeparator
+										+ "datadriven" + pathSeparator
+										+ dataDrivenJavaFileName + ".java"));
+						String packageString = packagePath + pathSeparator
+								+ "datadriven";
+						dataDrivenClassWriter(dataDrivenJavaFileName, output,
+								"RobotiumDataDrivenTemplate", testFilePath, packageString);
+						dataDrivenClasses += "test."
+								+ packageString
+										.replace(pathSeparator + "", ".")
+								+ "."+dataDrivenJavaFileName + ",";
+						output.close();
+
+					}
+
+				}
+				
+				
+
+			}
+            
+            //Java class generation for non-datadriven cases
+            else {
             try{
             packagePath = packagePath
                     .substring(0, packagePath.lastIndexOf(pathSeparator));}
@@ -142,14 +195,62 @@ public class TestCaseGenerator {
 
             classWriter(classPath, ar, output,template);
             output.close();
+            }
         }
 
     }
+    
+    //Class writer for data driven csv
+    private void dataDrivenClassWriter(String fileName, Writer output,
+			String template,String testFilePath,String packagePath) {
+    	
+    	try{
+    	char pathSeparator=File.separatorChar;
+    	FileInputStream fstream;
+    	String resourcesPath=baseFolder+"resources"+pathSeparator;
+    	fstream = new FileInputStream(resourcesPath+template);
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        boolean flag = false;
+        String importString=packagePath.replace(pathSeparator+"", ".");
+        String javaClass ="package test."+importString+";\n";
+        String tempString="";
+        while ((tempString = br.readLine()) != null) {
+            if (tempString.contains("TestClassName")){
+            		javaClass+=tempString.replace("TestClassName", fileName)+"\n";
+            }
+            else if (tempString.contains("filePath")){
+        		javaClass+=tempString.replace("filePath", testFilePath)+"\n";
+        		
+            }
+            else if (tempString.contains("dataFile")){
+            	String dataFile=testFilePath.substring(0,testFilePath.indexOf("_datadriven.csv"));
+            
+            	javaClass+=tempString.replace("dataFile", dataFile+"_data.csv")+"\n";
+            }
+            else{
+            	javaClass+=tempString+"\n";
+            }
 
-    public static void main(String[] args) {
+
+        }
+       
+        output.write(javaClass);
+    	}
+    	catch(IOException e) {
+    		
+    		System.out.println("Exception  "+e);
+    		
+    	}
+    	
+		
+	}
+
+	public static void main(String[] args) {
         TestCaseGenerator tc = new TestCaseGenerator();
         HashMap<String, ArrayList<String>> hm1 = new HashMap<String, ArrayList<String>>();
         DefaultProperties prop = DefaultProperties.getDefaultProperty();
+        boolean absolute=false;
         boolean isRobotium=false;
         String template ="Template";
         char pathSeparator=File.separatorChar;
@@ -160,21 +261,18 @@ public class TestCaseGenerator {
             isRobotium=true;
             template="RobotiumTemplate";
         }
-        if(isRobotium){
-            tc.listAbsoluteDirectory(new File(testFolderPath
-                    + prop.getValueFromProperty("TESTCASE_FOLDER")), "",
-            hm1);
+        if(!isRobotium){
+            absolute=true;
         }
-        else{
-            tc.listDirectory(
-                    new File(testFolderPath
-                            + prop.getValueFromProperty("TESTCASE_FOLDER")), "",
-                    hm1);
-
-        }
+        tc.listDirectory(new File(testFolderPath
+                + prop.getValueFromProperty("TESTCASE_FOLDER")), "",hm1, "csv", absolute);
         System.out.println(hm1);
         try {
             tc.classGenerator(hm1,template);
+            //Creating a properties file specifying data driven classes 
+            Writer dataDrivenPropertiesWriter = new BufferedWriter(new FileWriter(baseFolder+"resources"+pathSeparator+"datadriven.properties"));
+			dataDrivenPropertiesWriter.write("DATA_CLASSES="+tc.dataDrivenClasses);
+			dataDrivenPropertiesWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
