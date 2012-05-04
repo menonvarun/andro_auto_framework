6dcc8c2fb474e978764542a44919ddd2f2fdc1e4
Moiz Virani <moizjv@gmail.com>
2012-05-04 19:58:43 +0530

    Adding feature data driven with example of word press

---
+++ runner/src/org/imaginea/botbot/common/AndroFrameworkExecutorDataDriven.java
@@ -0,0 +1,66 @@
+package org.imaginea.botbot.common;
+
+import java.io.InputStream;
+import java.util.HashMap;
+
+import junit.framework.Assert;
+
+import org.imaginea.botbot.api.TestCSVReader;
+import org.imaginea.botbot.keywords.KeywordCaller;
+
+import android.util.Log;
+
+public class AndroFrameworkExecutorDataDriven extends AndroFrameworkExecutor{
+	
+	
+	public AndroFrameworkExecutorDataDriven(Prefrences prefrences){
+		super(prefrences);
+	}
+	
+	//For Data driven test cases 
+	
+		public void androExecutor(InputStream filePath,HashMap<String, String> testData) {
+			kc = new KeywordCaller(prefrences);
+			try {
+				reader = new TestCSVReader(filePath);
+			} catch (Exception e) {
+				Assert.fail("Unable to read the csv file :"+ e.toString());
+			}
+			this.execute(testData);
+		}
+		
+		//For Data driven test cases executing test cases with putting data in paramters. 
+		
+		private void execute(HashMap<String, String> testData){
+			Log.e("Keys", testData.keySet().toString());
+			String[] parameters;
+			int rows = reader.getLines();
+			for (int i = 1; i < rows; i++) {
+				Command command = new Command();
+				String[] row =reader.getRow(i);
+				if(row.length<=1){
+					parameters=new String[0];
+				}else if(row.length==2 && row[1]==""){
+					parameters=new String[0];
+				}else{
+					for(int count=0;count<row.length;count++)
+					{
+						if(row[count].startsWith("$"))
+						{				
+							Log.e("Parameters",row[count].substring(1));
+							row[count]=testData.get(row[count].substring(1));
+							
+						
+						}
+					}
+					parameters=new String[row.length-1];
+					System.arraycopy(row, 1, parameters, 0, row.length-1);
+				}
+				
+				command.setName(row[0]);
+				command.setParameters(parameters);
+				kc.execute(command);
+			}
+		}
+
+}
---
+++ runner/src/org/imaginea/botbot/utility/DataDrivenDataGenerator.java
@@ -0,0 +1,56 @@
+package org.imaginea.botbot.utility;
+
+import java.util.HashMap;
+import java.util.List;
+import junit.framework.Assert;
+import org.imaginea.botbot.api.TestCSVReader;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+public class DataDrivenDataGenerator {
+
+	String csvFilePath;
+	List<String[]> row = null;
+	String[] keySet = null;
+	TestCSVReader csvReader = null;
+
+	static Context targetContext;
+
+	public static void setTargetContext(Context targetContext) {
+		DataDrivenDataGenerator.targetContext = targetContext;
+	}
+
+	public DataDrivenDataGenerator(String csvFilePath) {
+		this.csvFilePath = csvFilePath;
+		try {
+			Log.e("context", targetContext.getPackageName());
+			Resources res = targetContext.getResources();
+
+			AssetManager asset = res.getAssets();
+			csvReader = new TestCSVReader(asset.open(csvFilePath));
+
+		} catch (Exception e) {
+			Log.e("csv cannot be read", e + " CSV");
+			Assert.fail("Unable to read the csv file :" + e.toString());
+		}
+		keySet = csvReader.getRow(0);
+
+	}
+
+	public HashMap<String, String> getHashMapForRow(int rowNumber) {
+		HashMap<String, String> data = new HashMap<String, String>();
+		for (int count = 0; count < keySet.length; count++) {
+			data.put(keySet[count], csvReader.getData(rowNumber, count));
+		}
+		return data;
+
+	}
+
+	public int getNumberOfRows() {
+		// Subtracting 1 because first row is of keySet
+		return csvReader.getLines() - 1;
+	}
+
+}
